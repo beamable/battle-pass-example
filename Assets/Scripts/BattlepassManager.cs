333c21ba4e9c98f60d69abdcd524a7a79b591d45
GeorgianaIonUPD <georgiana.ion@updivision.com>
2024-10-01 13:24:51 +0300

    Save batlepass to inventory

--- Assets/Scripts/BattlepassManager.cs
+++ Assets/Scripts/BattlepassManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Beamable;
 using Beamable.Common.Content;
 using UnityEngine;
@@ -8,12 +10,13 @@ namespace DefaultNamespace
     {
         [SerializeField] private ContentRef<Battlepass> _battlepassRef;
         private Battlepass _battlepass;
+        private BeamContext _beamContext;
 
         private async void Start()
         {
             // Get the Beamable context
-            var beamContext = BeamContext.Default;
-            await beamContext.OnReady;
+            _beamContext = await BeamContext.Default.Instance;
+            Debug.Log(_beamContext.PlayerId);
 
             // Fetch the Battlepass content
             await _battlepassRef.Resolve()
@@ -40,6 +43,28 @@ namespace DefaultNamespace
                     Debug.Log($"  Reward: {reward.RewardName}, Quantity: {reward.Quantity}");
                 }
             }
+        }
+        
+        // Method to add Battlepass to player's inventory
+        private async Task AddBattlepassToInventory()
+        {
+            _beamContext = await BeamContext.Default.Instance;
+
+            var inventory = _beamContext.Inventory;
+
+            // Use Update to add the Battlepass to the player's inventory
+            await inventory.Update(builder => builder.AddItem("items.battlepass", new Dictionary<string, string>
+            {
+                { "name", _battlepass.Name },  // You can add any properties related to the battle pass here
+                { "validityPeriod", "30" }     // Example property for the battle pass duration (in days)
+            }));
+
+            Debug.Log("Battlepass added to inventory!");
+        }
+
+        public async void GetBattlepassButton()
+        {
+            await AddBattlepassToInventory();
         }
     }
 }
